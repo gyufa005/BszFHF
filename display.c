3377c167962dfe37a738799979293658ec29b28b
gyufa005 <gyu.fa005@gmail.com>
2024-11-11 16:39:23 +0100

    Próbáltam koordinátázni, ez így nem lesz jó, ezért állapotgépezni fogok

--- display.c
+++ display.c
@@ -9,6 +9,19 @@
 #include "segmentlcd.h"
 #include "segmentlcd_individual.h"
 
+//2 féle irány létezik --> horizontálisnál
+
+SegmentLCD_LowerCharSegments_TypeDef lowerCharSegments[SEGMENT_LCD_NUM_OF_LOWER_CHARS][5]; //also szegmensek tombjet reprezentalo tomb
+typedef enum {HORIZONTAL,VERTICAL} Alignment;
+typedef struct {
+  uint8_t x;
+  uint8_t y;
+}Position;
+typedef struct {
+  uint8_t segnum; //which segment is on
+  Position s_position; //which position in the segment is on
+};
+
 sl_sleeptimer_timer_handle_t timer;
 static void timeout_callback(sl_sleeptimer_timer_handle_t *handle,void *data);
 
@@ -20,3 +33,4 @@ timeout_callback(sl_sleeptimer_timer_handle_t * handle, void * data){
   //Mit csinál mintavételkor
 }
 //TODO : kirajzolás függvény, és ahhoz a változók struktúrája???
+void MoveHead
